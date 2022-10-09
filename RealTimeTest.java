1891137f5e6c66397069648c73d1c0642e716a06
Darshana Perera <darshana.saluka.10@gmail.com>
2022-10-09 10:39:30 +0530

    web driver library object created

---
+++ RealTimeTest.java
@@ -0,0 +1,10 @@
+package realtimeTest;
+
+import org.openqa.selenium.WebDriver;
+
+public class RealTimeTest {
+	public static void main(String []args) {
+		String url = "https://www.ebay.com/";
+		WebDriver driver;
+	}
+}
