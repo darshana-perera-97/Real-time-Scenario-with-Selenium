83a997a145ef2d00bb9bc979b77288da5c88e647
Darshana Perera <darshana.saluka.10@gmail.com>
2022-10-09 10:45:09 +0530

    web site opening code created

--- RealTimeTest.java
+++ RealTimeTest.java
@@ -1,10 +1,23 @@
 package realtimeTest;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class RealTimeTest {
+	String url = "https://www.ebay.com/";
+	WebDriver driver = new ChromeDriver();;
 	public static void main(String []args) {
-		String url = "https://www.ebay.com/";
-		WebDriver driver;
+		System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+		
 	}
+	public void openSiteOnBrowser() {
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+		driver.get(url);
+	}
+	
+	
 }
