ab78e011688467381a50ed935e1e6ba854bdc601
Darshana Perera <darshana.saluka.10@gmail.com>
2022-10-09 10:51:00 +0530

    open site and search

--- RealTimeTest.java
+++ RealTimeTest.java
@@ -2,7 +2,9 @@ package realtimeTest;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class RealTimeTest {
@@ -10,6 +12,10 @@ public class RealTimeTest {
 	WebDriver driver = new ChromeDriver();;
 	public static void main(String []args) {
 		System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+		RealTimeTest realTimeTest = new RealTimeTest();
+		realTimeTest.openSiteOnBrowser();
+		realTimeTest.searchProduct("Apple Watches", " Jewelry & Watches");
+		
 		
 	}
 	public void openSiteOnBrowser() {
@@ -19,5 +25,11 @@ public class RealTimeTest {
 		driver.get(url);
 	}
 	
+	public void searchProduct(String product, String category) {
+		WebElement search = driver.findElement(By.id("gh-ac"));
+		search.sendKeys(product);
+		driver.findElement(By.id("gh-cat")).sendKeys(category);
+		driver.findElement(By.id("gh-btn")).click();
+	}
 	
 }
